8c59007d90d249c393d45cf39d8f50666e900b5e
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-10-19 14:35:51 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,16 @@ import "./style.css";
 const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+function AddHTMLElement(type: string): HTMLElement{
+    const tmp = document.createElement(type);
+    app.append(tmp);
+    return tmp;
+}
+
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const header = AddHTMLElement("h1");
+header.innerHTML = APP_NAME;
+const canvas = AddHTMLElement("canvas");
+canvas.id = "canvas";
+canvas.style.width = "256px";
+canvas.style.height = "256px";
