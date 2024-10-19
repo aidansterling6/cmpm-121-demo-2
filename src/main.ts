bd3fa7fc9a8bd21f4963295e80572daf3cd4acbf
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-10-19 15:33:28 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,49 @@ import "./style.css";
 const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-function AddHTMLElement(type: string): HTMLElement{
+function AddHTMLElement(type: string, innerHTML: string | null = null): HTMLElement{
     const tmp = document.createElement(type);
     app.append(tmp);
+    if(innerHTML !== null){
+        tmp.innerHTML = innerHTML;
+    }
     return tmp;
 }
 
 document.title = APP_NAME;
-const header = AddHTMLElement("h1");
-header.innerHTML = APP_NAME;
-const canvas = AddHTMLElement("canvas");
+const header = AddHTMLElement("h1", APP_NAME);
+const canvas = document.createElement("canvas");
+app.append(canvas);
 canvas.id = "canvas";
-canvas.style.width = "256px";
-canvas.style.height = "256px";
+canvas.width = 256;
+canvas.height = 256;
+const clearButton = AddHTMLElement("button", "clear");
+const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+let lastx = 0;
+let lasty = 0;
+let mouseDown = false;
+canvas.addEventListener("mousedown", (event) => {
+    mouseDown = true;
+});
+canvas.addEventListener("mouseup", (event) => {
+    mouseDown = false;
+});
+canvas.addEventListener("mousemove", (event) => {
+    if(ctx !== null && mouseDown){
+        DrawLine(ctx, lastx, lasty, event.offsetX, event.offsetY);
+    }
+    lastx = event.offsetX;
+    lasty = event.offsetY;
+});
+clearButton.addEventListener("click", (event) => {
+    if(ctx !== null){
+        ctx.reset();
+    }
+});
+
+function DrawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number){
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
